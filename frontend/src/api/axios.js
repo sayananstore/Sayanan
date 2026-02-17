75b77f97bbf57e3127e7797fee72dbc087f0d286
25bhavana <soge.bhavana@gmail.com>
2026-02-18 01:21:52 +0530

    Backend Url Update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://sayanan.onrender.com/api",
   headers: {
     "Cache-Control": "no-cache",
   },
